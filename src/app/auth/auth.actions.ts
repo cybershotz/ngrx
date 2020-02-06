03633420fd0b8c8c92b6a069e5c035cf6443c63e
Ammar <ammar4568@gmail.com>
2020-02-06 19:44:50 +0500

    4. Dispatching an Action - Implementing the Login Component

--- src/app/auth/auth.actions.ts
+++ src/app/auth/auth.actions.ts
@@ -1,14 +1,19 @@
 import { Action } from '@ngrx/store';
+import { User } from '../model/user.model';
 
 export enum AuthActionTypes {
   LoginAction = '[Login] Action',
   LogoutAction = '[Logout] Action',
-  
-  
+
+
 }
 
 export class Login implements Action {
   readonly type = AuthActionTypes.LoginAction;
+
+  constructor(private payload: { user: User }) {
+
+  }
 }
 
 
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -36,8 +36,21 @@ export class LoginComponent implements OnInit {
 
   }
 
-  login() {
-    this.store.dispatch(new Login())
+  login() { 
+    const val = this.form.value;
+
+    this.auth.login(val.email, val.password)
+    .pipe(
+      tap(user => {
+        this.store.dispatch(new Login({user}));
+
+        this.router.navigateByUrl('/courses')
+      })
+    )
+    .subscribe(
+      noop,
+      () => alert('login failed')
+    )
   }
 
 
